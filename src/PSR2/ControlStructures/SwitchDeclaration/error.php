74d54ca7c898cb4604d8c6ca0809e1a20e138f60
blue32a <blue32a.jp@gmail.com>
2023-05-18 04:44:35 +0000

    PSR2.ControlStructures.SwitchDeclaration

---
+++ src/PSR2/ControlStructures/SwitchDeclaration/error.php
@@ -0,0 +1,13 @@
+<?php
+
+switch ($expr) {
+    case   1:
+        echo 'First case';
+        break;
+    Case 2:
+        echo 'Second case';
+        break;
+    Default:
+        echo 'Default case';
+            break;
+}
---
+++ src/PSR2/ControlStructures/SwitchDeclaration/fixed.php
@@ -0,0 +1,13 @@
+<?php
+
+switch ($expr) {
+    case 1:
+        echo 'First case';
+        break;
+    case 2:
+        echo 'Second case';
+        break;
+    default:
+        echo 'Default case';
+        break;
+}
